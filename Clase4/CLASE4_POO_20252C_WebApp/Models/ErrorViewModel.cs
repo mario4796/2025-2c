293eb02b57e17e19c15aba739d90b173ed796b56
mario4796 <mario.checchia@outlook.com>
2025-10-26 15:50:31 -0300

    se agrego tarea 6

--- Clase4/CLASE4_POO_20252C_WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CLASE4_POO_20252C_WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
